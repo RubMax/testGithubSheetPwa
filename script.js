fdf20bdd1e5fcfef6b658681b19d739aee5a493c
RubMax <54785672+RubMax@users.noreply.github.com>
2025-07-11 08:16:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ fetch(csvURL)
   .then(data => {
     const lines = data.split('\n');
     const a2Value = lines[1].split(',')[0]; // A2 = première colonne de la 2e ligne
-    const a3Value = lines[2].split(',')[1];
+    const a3Value = lines[1].split(',')[1];
     document.getElementById('entreprise').textContent = a2Value || 'Nom introuvable';
-  document.getElementById('entreprise').textContent = a2Value || 'Nom introuvable';
+  document.getElementById('entreprise').textContent = a3Value || 'Nom introuvable';
   })
   .catch(err => {
     document.getElementById('entreprise').textContent = 'Erreur de chargement';
